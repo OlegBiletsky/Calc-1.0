2712feccc189200cfbaf57c845ffb5d53a900e6f
OlegBiletsky <ol.biletsky@gmail.com>
2020-04-15 11:36:41 +0300

    ONLY +.         Input numbers must be canceled by mouse and keyboard. Result only click by mouse

--- Calc-1.0/js/main.js
+++ Calc-1.0/js/main.js
@@ -1,10 +1,21 @@
-var buttonPlus = document.getElementById(id = "buttonPlus");
+var buttonPlus = document.getElementById("buttonPlus");
 var buttonMinus = document.getElementById("buttonMinus");
 var buttonMultiply = document.getElementById("buttonMultiply");
 var buttonDevide = document.getElementById("buttonDevide");
 
 function onButtonPlusClick() {
     console.log("onButtonPlusClick");
+    var firstNumber = document.getElementById("first-number");
+    var secondNumber = document.getElementById("second-number");
+
+    console.log(firstNumber.value);
+    console.log(secondNumber.value);
+    
+    var one = Number (firstNumber.value);
+    var two = Number (secondNumber.value);
+
+    console.log(one + two);
+    window.alert(one + two);
 }
 
 function onButtonMinusClick() {
@@ -27,8 +38,23 @@ function onButtonDevideClick() {
 // else (window.alert("00000"))
 
 buttonPlus.addEventListener("click",onButtonPlusClick);
+buttonMinus.addEventListener("click", onButtonMinusClick);
+buttonMultiply.addEventListener("click", onButtonMultiplyClick);
+buttonDevide.addEventListener("click", onButtonDevideClick);
 
 
 
+
+
+
+
+
+
+
+// но сейчас к калькулятору, сделанному по этому видео, я добавил
+//кнопку очистить, которая выставляет значения всех инпутов 
+//на " ", также я сделал третий инпут и прописал в функциях чтобы
+//решение  примера выводилось не в window.alert, а в третьий 
+//инпут
 
 
