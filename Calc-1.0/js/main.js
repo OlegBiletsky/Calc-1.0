73103395a8b1bb540a76759cb7ef0446dbd5bdbf
OlegBiletsky <ol.biletsky@gmail.com>
2020-04-15 10:21:15 +0300

    Fixed including javascript file to index.html. J.script include in down

--- Calc-1.0/js/main.js
+++ Calc-1.0/js/main.js
@@ -1,32 +1,33 @@
 var buttonPlus = document.getElementById(id = "buttonPlus");
-var buttonMinus = document.getElementById(id = "buttonMinus");
-var buttonMultiply = document.getElementById(id = "buttonMultiply");
-var buttonDevide = document.getElementById(id = "buttonDevide");
+var buttonMinus = document.getElementById("buttonMinus");
+var buttonMultiply = document.getElementById("buttonMultiply");
+var buttonDevide = document.getElementById("buttonDevide");
 
 function onButtonPlusClick() {
     console.log("onButtonPlusClick");
 }
 
-window.alert(buttonPlus);
+function onButtonMinusClick() {
+    console.log("onButtonMinusClick");
+}
+
+function onButtonMultiplyClick() {
+    console.log("onButtonMultiplyClick");
+}
+
+function onButtonDevideClick() {
+    console.log("onButtonDevideClick");
+}
 
 
-// function onButtonMinusClick() {
-//     console.log("onButtonMinusClick");
-// }
 
-// function onButtonMultiplyClick() {
-//     console.log("onButtonMultiplyClick");
-// }
-
-// function onButtonDevideClick() {
-//     console.log("onButtonDevideClick");
-// }
 // if (buttonPlus !== null) {
 //     buttonPlus.addEventListener("onclick",onButtonPlusClick);
 // }
 // else (window.alert("00000"))
 
-buttonPlus.addEventListener("onclick",onButtonPlusClick);
+buttonPlus.addEventListener("click",onButtonPlusClick);
+
 
 
 
